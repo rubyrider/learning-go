11b49ad93eb86345157cb3f1f18563c0adb96b50
Irfan Ahmed <irfandhk@gmail.com>
2020-08-22 18:28:27 +0200

    add: Basic variables

---
+++ basics/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i = "Hi, Irfan"
+	fmt.Println(i)
+
+	var j int = 10
+	fmt.Println(j)
+
+	var k,l int = 15, 30
+	fmt.Println("j+k-l=", j+k-l)
+
+	a := "Bye!"
+	fmt.Println(a)
+}
